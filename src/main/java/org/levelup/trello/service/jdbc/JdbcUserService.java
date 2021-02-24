6ca9f64886059f23b9f9c613c8d0c64630a9e8c5
Alexander_Reztsov <alexander_reztsov@mail.ru>
2021-02-24 21:27:14 +0300

    added TerminalService minor readme changes some other changes

--- src/main/java/org/levelup/trello/service/jdbc/JdbcUserService.java
+++ src/main/java/org/levelup/trello/service/jdbc/JdbcUserService.java
@@ -1,27 +1,34 @@
 package org.levelup.trello.service.jdbc;
-import org.levelup.trello.jdbc.JdbcConnectionService;
+import lombok.SneakyThrows;
 import org.levelup.trello.model.User;
 import org.levelup.trello.service.UserService;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class JdbcUserService implements UserService {
 
     private final JdbcConnectionService jdbcConnectionService;
+    private final Connection connection;
 
+    @SneakyThrows
     public JdbcUserService(){
         this.jdbcConnectionService = new JdbcConnectionService();
+        connection = jdbcConnectionService.openConnection();
     }
 
     @Override
     public User createUser(String name, String login, String email, String password) {
 
         try (Connection connection = jdbcConnectionService.openConnection()){
-            String sql = "insert into users (id, name, login, email) values (nextval('user_id_sequence')?, ?, ?)";
+
+            String sql = "insert into users (name, login, email) values (?, ?, ?)";
             PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, login);
-            stmt.setString(2, email);
-            stmt.setString(3, name);
+            stmt.setString(1, name);
+            stmt.setString(2, login);
+            stmt.setString(3, email);
 
             int rowAffected = stmt.executeUpdate();
             System.out.println(rowAffected + " row affected");
@@ -29,29 +36,63 @@ public class JdbcUserService implements UserService {
             ResultSet keys = stmt.getGeneratedKeys();
             keys.next();
             int generatedID = keys.getInt(1);
-            System.out.println("User ID " + generatedID);
 
             saveUserCredentials(connection, generatedID, password);
 
             return new User(generatedID, login, email, name);
 
-        } catch (SQLException exc) {
-            System.out.println("Ошибка при работе с базой " + exc.getMessage());
-            System.out.println();
-            exc.printStackTrace();
-            throw new RuntimeException(exc);
-
+        } catch (SQLException e) {
+            System.out.println("Ошибка при работе с базой");
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public ArrayList<User> showAllUsers(){
+        ArrayList<User> userList = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from users");
+            while(rs.next()){
+                Integer id = rs.getInt(1);
+                String name = rs.getString(2);
+                String login = rs.getString(3);
+                String email = rs.getString(4);
 
+                userList.add(new User(id, name, login, email));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
 
-    private void saveUserCredentials(Connection connection, Integer userId, String password) throws SQLException {
-        PreparedStatement stmt = connection.prepareStatement("insert into user_credentials valuee (?, ?)");
+        return userList;
+    }
+
+    @SneakyThrows
+    private void saveUserCredentials(Connection connection, Integer userId, String password){
+        PreparedStatement stmt = connection.prepareStatement("insert into user_credentials values (?, ?)");
         stmt.setInt(1, userId);
         stmt.setString(2, password);
         stmt.executeUpdate();
-
     }
 
+    @SneakyThrows
+    public boolean signIn(String login, String password){
+       PreparedStatement ps = connection.prepareStatement("SELECT login, password " +
+               "from user_credentials a " +
+               "inner join users b " +
+               "on a.user_id = b.id " +
+               "where login = ? and password = ?");
+       ps.setString(1, login);
+       ps.setString(2, password);
+       ResultSet rs = ps.executeQuery();
+       if (rs.next() == false){
+           System.out.println("wrong login or password");
+           return false;
+       }
+       else {
+           return true;
+       }
+    }
 }
