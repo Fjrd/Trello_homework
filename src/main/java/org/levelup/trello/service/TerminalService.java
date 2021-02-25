795da9a6a133a7fe96eb4f94bebd6fc009475dbd
Alexander_Reztsov <alexander_reztsov@mail.ru>
2021-02-25 06:47:59 +0300

    added some board management

--- src/main/java/org/levelup/trello/service/TerminalService.java
+++ src/main/java/org/levelup/trello/service/TerminalService.java
@@ -1,7 +1,8 @@
 package org.levelup.trello.service;
 
 import lombok.SneakyThrows;
-import org.levelup.trello.service.UserService;
+import org.levelup.trello.model.Board;
+import org.levelup.trello.model.User;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.io.InputStreamReader;
 public class TerminalService {
 
     private UserService userService;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public TerminalService(UserService userService) {
         this.userService = userService;
@@ -17,19 +19,22 @@ public class TerminalService {
 
     @SneakyThrows
     public void startUp(){
-        System.out.println("Welcome stranger!");
-        System.out.println("If you are registered user, please sign in. Type \"log in\" or \"sign up\"");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome!");
+        System.out.println("1 - log in");
+        System.out.println("2 - sign up");
+        System.out.println("3 - help");
+        System.out.println("4 - exit");
         while (true){
-            String command = br.readLine();
+            int command = Integer.parseInt(br.readLine());
             switch (command){
-                case ("sign up"):{
-                    signUp();
+                case 1 : signIn();
                     break;
-                }
-                case("log in"):{
-                    signIn();
-                }
+                case 2 : signUp();
+                    break;
+                case 3 : startUp();
+                    break;
+                case 4 : System.exit(0);
+                    break;
             }
         }
     }
@@ -39,21 +44,18 @@ public class TerminalService {
         String login;
         String password;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("login:");
+        System.out.println();
+        System.out.println("Insert your login:");
         login = br.readLine();
-        System.out.println("password:");
+        System.out.println("Insert your password:");
         password = br.readLine();
-        if (userService.signIn(login, password))
-            System.out.println("User found! Now you can manage you boards!");
-    }
-
-    public void printAllUsers(){
-        userService.showAllUsers().forEach(System.out::println);
+        Integer userId = userService.signIn(login, password);
+        if (userId != 0)
+            userMenu(userId);
     }
 
     public void signUp(){
         String name, login, email, password;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
             System.out.println("Create new user:");
@@ -71,11 +73,76 @@ public class TerminalService {
             password = br.readLine();
 
             userService.createUser(name, login, email, password);
-            System.out.println("User creation success");
 
         } catch (IOException e) {
             System.out.println("Wrong value, try again");
             signUp();
         }
     }
+
+    @SneakyThrows
+    private void userMenu(Integer id) {
+        User user = userService.showUser(id);
+        System.out.println("User found! Now you can manage your boards!");
+        System.out.println("1 - Manage your boards");
+        System.out.println("2 - Exit");
+        while (true){
+            int command = Integer.parseInt(br.readLine());
+            switch (command){
+                case 1 : manageBoards(user);
+                    break;
+                case 2 : startUp();
+                    break;
+            }
+        }
+    }
+
+    @SneakyThrows
+    private void manageBoards(User user) {
+        System.out.println("1 - Show board list");
+        System.out.println("2 - Add new board");
+        System.out.println("3 - Edit board");
+        System.out.println("4 - Delete board");
+        while (true){
+            int command = Integer.parseInt(br.readLine());
+            switch (command){
+                case 1 : printUserBoards(user);
+                    break;
+                case 2 : addNewBoard(user);
+                    break;
+                case 3 :
+                    break;
+                case 4 : deleteBoard(user);
+                    break;
+            }
+        }
+    }
+
+    private void deleteBoard(User user) {
+        System.out.println("which board do you want to remove?");
+
+    }
+
+    @SneakyThrows
+    private void addNewBoard(User user) {
+        System.out.println("New board:");
+        String name = br.readLine();
+        Boolean favourite = Boolean.valueOf(br.readLine());
+        Integer userId = user.getId();
+        userService.addNewBoard(name, favourite, userId);
+    }
+
+    private void printUserBoards(User user) {
+        System.out.println("This is all your boards:");
+        for (Board board : userService.showAllUserBoards(user.getId())) {
+            System.out.println(board.toString());
+        }
+
+    }
+
+    public void printAllUsers(){
+        userService.showAllUsers().forEach(System.out::println);
+    }
+
+
 }
--- src/main/java/org/levelup/trello/service/UserService.java
+++ src/main/java/org/levelup/trello/service/UserService.java
@@ -1,5 +1,6 @@
 package org.levelup.trello.service;
 
+import org.levelup.trello.model.Board;
 import org.levelup.trello.model.User;
 
 import java.util.ArrayList;
@@ -7,5 +8,12 @@ import java.util.ArrayList;
 public interface UserService {
     User createUser(String name, String login, String email, String password);
     ArrayList<User> showAllUsers();
-    boolean signIn(String login, String password);
+    User showUser(Integer id);
+    Integer signIn(String login, String password);
+    ArrayList<Board> showAllUserBoards(Integer userId);
+    void deleteBoard(Integer id);
+    Board editBoard(Integer id);
+    Board addNewBoard(String name, Boolean favourite, Integer userId);
+
+
 }
