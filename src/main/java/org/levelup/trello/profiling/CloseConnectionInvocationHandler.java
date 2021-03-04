c114884075157ffc9a85f9867590970bbb3806bf
Dmitry Protsko <dmitr.prot@gmail.com>
2021-03-04 22:23:26 +0300

    Code from lesson 6

--- src/main/java/org/levelup/trello/profiling/CloseConnectionInvocationHandler.java
+++ src/main/java/org/levelup/trello/profiling/CloseConnectionInvocationHandler.java
@@ -1,4 +1,23 @@
 package org.levelup.trello.profiling;
 
-public class CloseConnectionInvocationHandler {
+import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+
+@RequiredArgsConstructor
+public class CloseConnectionInvocationHandler implements InvocationHandler {
+
+    private final Connection connection;
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getName().equals("close")) {
+                ConnectionAliveTimeService.getInstance()
+                    .printConnectionAliveTime(connection);
+        }
+        return method.invoke(connection, args);
+    }
+
 }
