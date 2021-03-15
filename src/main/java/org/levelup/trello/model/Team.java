c09e0b82f65ecf60ab57dda5bbcd6f2e8dc9e24b
Dmitry Protsko <dmitr.prot@gmail.com>
2021-03-15 22:02:53 +0300

    Code from lesson 8

--- src/main/java/org/levelup/trello/model/Team.java
+++ src/main/java/org/levelup/trello/model/Team.java
@@ -4,7 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Collection;
 
 @Setter
--- src/main/java/org/levelup/trello/model/User.java
+++ src/main/java/org/levelup/trello/model/User.java
@@ -1,9 +1,9 @@
 package org.levelup.trello.model;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -11,17 +11,24 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
 
 //@Data
-//@Setter
+@Setter
 @Getter
 @Entity
 @Table(name = "users")
 @ToString
 @EqualsAndHashCode
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id // primary key из таблицы users
@@ -32,5 +39,26 @@ public class User {
     @Column(name = "login")
     private String login;
     private String email;
+    @OneToMany(mappedBy = "owner") // mappedBy прописывается только в случае bidirectional связи
+    private Collection<Board> boards;
+    @OneToOne
+    @MapsId
+    private UserCredentials credentials;
+    @ManyToMany
+    @JoinTable(
+            name = "team_member",
+            joinColumns = @JoinColumn(name = "user_id"), // колонка из таблицы team_member, которая ссылается на Id User
+            inverseJoinColumns = @JoinColumn(name = "team_id") // колонка из таблицы team_member, которая ссылается на Id Team
+    )
+    private Collection<Team> teams;
+
+    public User(Integer id, String name, String login, String email) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.email = email;
+        this.boards = new ArrayList<>();
+        this.teams = new ArrayList<>();
+    }
 
 }
